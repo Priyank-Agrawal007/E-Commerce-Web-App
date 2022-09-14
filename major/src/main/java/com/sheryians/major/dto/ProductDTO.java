810114f85726fac431e7d55eb5f99fb70f4bd4b8
Priyank-Agrawal007 <97780557+Priyank-Agrawal007@users.noreply.github.com>
2022-09-14 16:40:12 +0530

    Add files via upload

---
+++ major/src/main/java/com/sheryians/major/dto/ProductDTO.java
@@ -0,0 +1,23 @@
+package com.sheryians.major.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductDTO {
+    
+    private Long id;
+
+    private String name;
+
+    
+    private int categoryId;
+
+    private Double price;
+
+    private Double weight;
+
+    private String description;
+
+    private String imageName;
+
+}
